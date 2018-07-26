dc7c7e7165d50d39149187f1d90a8a5b31e94651
rishi1tripathi <41673939+rishi1tripathi@users.noreply.github.com>
2018-07-26 12:09:36 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,19 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import java.util.Scanner;
+ 
+class MultiplicationTable
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "my first java code!" );
-    }
+   public static void main(String args[])
+   {
+      int n, c;
+      System.out.println("Enter an integer to print it's multiplication table");
+      Scanner in = new Scanner(System.in);
+      n = in.nextInt();
+      System.out.println("Multiplication table of " + n + " is : ");
+ 
+      for (c = 1; c <= 10; c++)
+         System.out.println(n + "*" + c + " = " + (n*c));
+   }
 }
